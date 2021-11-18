1d04dedc22a87b48733fdc0ac0570ae69bd2846f
Alexidis911 <Alexidis911@gmail.com>
2021-11-18 22:19:32 +0300

    Использование макрокоманды

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from game import Tank, Move, Rotate, MovableAdapter, RotatableAdapter
+from game import Tank, Move, Rotate, CheckFuel, BurnFuel, \
+                 BurnFuelMove, MovableAdapter, RotatableAdapter, CheckFuelAdapter, BurnFuelAdapter
 
 
 if __name__ == '__main__':
@@ -14,7 +15,8 @@ if __name__ == '__main__':
             break
         if cmd_name == 'move':
             T1.velocity = [int(i) for i in input('Через запятую введите введите вектор скорости\n').split(',')]
-            Move(MovableAdapter(T1)).execute()
+            cm_l = [CheckFuel(CheckFuelAdapter(T1)), Move(MovableAdapter(T1)), BurnFuel(BurnFuelAdapter(T1))]
+            BurnFuelMove(cm_l).execute()
             
         if cmd_name == 'rotate':
             T1.angle_velocity = int(input('Введите угол поворота\n'))
