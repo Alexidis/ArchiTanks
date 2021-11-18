145ad36c308824e34ec64864ccfd49b79fc10e12
Alexidis911 <Alexidis911@gmail.com>
2021-11-18 22:15:18 +0300

    Добавил поля для сжигания топлива

--- game/game_objects/Tank.py
+++ game/game_objects/Tank.py
@@ -3,11 +3,14 @@ from .UObject import UObject
 
 class Tank(UObject):
     
-    def __init__(self, position=None, direction=0):
+    def __init__(self, position=None, direction=0, fuel=100.0, fuel_velocity=43):
         self.position = position or [0, 0]
         self.velocity = [0, 0]
         self.direction = direction
         self.angle_velocity = 0
+        self.fuel = fuel
+        self.fuel_velocity = fuel_velocity
+        self._freeze()
         
     def get_property(self, prop_name: str):
         return self.__dict__[prop_name]
